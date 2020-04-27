19f4b1dbd9aaa0aa0a528ee7c36238cee353494d
Ian Herdegen <ianherdegen@ians-mbp.attlocal.net>
2020-04-27 15:35:53 -0400

    Finished

--- visualize_data.py
+++ visualize_data.py
@@ -6,13 +6,13 @@ def visualize_keywords():
     # Occurance of Top Keywords in Recent News
     keyword_data = pd.read_csv("keywords.csv")
     sns.barplot(x="keyword",y="occurance",data=keyword_data).set_title('Occurance of Top Keywords in Recent News')
-    plt.show()
+    plt.savefig('keywords.png')
 
 def visualize_lengths():
     # Categorical Representation of Playlist Lengths of all Playlists
     playlist_length_data = pd.read_csv("playlist_lengths.csv")
     sns.barplot(x="playlist_range",y="total",data=playlist_length_data).set_title('Categorical Representation of Playlist Lengths of all Playlists')
-    plt.show()
+    plt.savefig('playlist_ranges.png')
 
 def visualize_features():
     # Categorical Representation of Most Popular Feature among songs in Each Playlist
@@ -30,7 +30,7 @@ def visualize_features():
             shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     plt.title('Categorical Representation of Most Popular Features among all Playlists')
-    plt.show()
+    plt.savefig('features.png')
 
 def main():
     visualize_keywords()
